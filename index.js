be9f6d64f960cae7802f9502399ef8eaa4b6fc8d
Jaakko Hurtta <jaakko.hurtta@outlook.com>
2022-01-31 14:39:06 +0200

    part 13.4

--- index.js
+++ index.js
@@ -39,9 +39,35 @@ Blog.init(
   { sequelize, underscored: true, timestamps: false, modelName: "blog" },
 );
 
+// Mids
+app.use(express.json());
+
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  const newBlog = req.body;
+  const blog = await Blog.create(newBlog);
+  res.json(blog);
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    await Blog.destroy({ where: { id: req.params.id } });
+  } catch (e) {
+    res.status(404).end();
+  }
 });
 
 const PORT = process.env.PORT || 3001;
