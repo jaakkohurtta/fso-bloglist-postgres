c0479aa74073eecea0fbf3d316132a8eed5b6a1c
Jaakko Hurtta <jaakko.hurtta@outlook.com>
2022-01-31 13:51:21 +0200

    express app + blog model

---
+++ index.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import express from "express";
+import { Sequelize, Model, DataTypes } from "sequelize";
+
+const app = express();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  { sequelize, underscored: true, timestamps: false, modelName: "blog" },
+);
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
