5f76e63e8062310d728888a6f90aefb6a0e3700b
Jaakko Hurtta <jaakko.hurtta@outlook.com>
2022-02-08 09:45:58 +0200

    parts 13.13 - 13.14

--- controllers/blogs.mjs
+++ controllers/blogs.mjs
@@ -1,16 +1,37 @@
 import { Router } from "express";
+import { Op } from "sequelize";
 import { Blog, User } from "../models/index.mjs";
 import { blogFinder, tokenExtractor } from "../utils/middleware.mjs";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
+  let where = {};
+
+  if (req.query.search) {
+    where = {
+      [Op.or]: [
+        { author: { [Op.iLike]: `%${req.query.search}%` } },
+        { title: { [Op.iLike]: `%${req.query.search}%` } },
+      ],
+    };
+
+    /*
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`,
+    };
+    where.author = {
+      [Op.iLike]: `%${req.query.search}%`,
+    };
+    */
+  }
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name"],
     },
+    where,
   });
   res.json(blogs);
 });
